17cc801ffb82f8d308c4b64856a91dc8fc6905b9
Gennadiy <genaydzhan70@gmail.com>
2019-02-14 18:19:42 +0600

    Create Ask mode

--- app.rb
+++ app.rb
@@ -5,20 +5,35 @@ require "./lib/game.rb"
 enable :sessions
 
 get "/" do
-  session["game"] ||= Game.new
-  @game = session["game"]
-  redirect "/win" if @game.win
-  @guesses = @game.guesses
+  session.delete("game") if session["game"]
   erb :index
 end
 
-post "/index.html" do
-  session["game"].make_attempt(params["guess"])
-  redirect "/"
+get "/:mode" do
+  if params["mode"] == "pvc"
+    session["game"] = session["game"].class == PvC ? session["game"] : PvC.new
+  else
+    session["game"] = session["game"].class == CvP ? session["game"] : CvP.new
+  end
+  @game = session["game"]
+  redirect "/#{params["mode"]}/win" if @game.win
+  @guesses = @game.guesses
+  erb :game
 end
 
-get "/win" do
+post "/:mode" do
+  session["game"].make_attempt(params["guess"])
+  redirect "/#{params["mode"]}"
+end
+
+get "/pvc/win" do
   redirect "/" unless session["game"] && session["game"].win
   @game = session.delete("game")
   "Win! The answer is #{@game.code}!"
+end
+
+get "/cvp/win" do
+  redirect "/" unless session["game"] && session["game"].win
+  session.delete("game")
+  "Computer broke your code!"
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,53 @@ require "./lib/evaluate.rb"
 
 class Game
   include Evaluate
-  attr_reader :guesses, :code, :win
+  attr_reader :guesses, :win
 
   def initialize
     @guesses = []
-    @code = Array.new(4).map { rand(0..9) }.join
     @win = false
   end
 
   def make_attempt(guess)
     evaluation = evaluate(guess)
     @win = true if evaluation == "A4B0"
-    @guesses << {guess => evaluation}
+    @guesses << [guess, evaluation]
+  end
+end
+
+class PvC < Game
+  attr_reader :code
+
+  def initialize
+    super()
+    @code = Array.new(4).map { rand(0..9) }.join
+  end
+
+  def make_attempt(guess)
+    evaluation = evaluate(guess)
+    @guesses << [guess, evaluation]
+    @win = true if evaluation == "A4B0"
+  end
+end
+
+class CvP < Game
+  def initialize
+    super()
+    make_attempt
+  end
+
+  def make_attempt(evaluation = nil)
+    answers = (0..9999).map { |num| num.to_s.rjust(4, "0") }
+    if evaluation
+      if evaluation.upcase == "A4B0"
+        @win = true
+        return
+      end
+      @guesses.last[1] = evaluation.upcase
+      @guesses.each do |guess_record, evaluation_record|
+        answers.select! { |answer| evaluate(answer, guess_record) == evaluation_record }
+      end
+    end
+    @guesses << [answers.sample, nil]
   end
 end
