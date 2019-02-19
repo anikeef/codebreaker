9056be57f6c6c1e579b6280aa7d6e050f44dc572
Gennadiy <genaydzhan70@gmail.com>
2019-02-19 13:41:19 +0600

    Fix unwanted attempt bug

--- game.rb
+++ game.rb
@@ -25,9 +25,9 @@ class GuessGame < Game
     raise InvalidInput unless valid_input?(guess)
     evaluation = evaluate(guess)
     @history << [guess, evaluation]
+    @attempts_left -= 1
     @gameover_message = "Победа! Секретный код #{@code}!" if evaluation == "A4B0"
     @gameover_message = "У вас закончились попытки. Секретный код #{@code}" if @attempts_left == 0
-    @attempts_left -= 1
   end
 
   def valid_input?(input)
